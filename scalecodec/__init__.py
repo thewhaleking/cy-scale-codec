69682cb8334f92b9629309ec3276f77815471e43
BD Himes <b@latent.to>
2026-04-08 13:32:19 +0200

    Imports for linter

--- scalecodec/__init__.py
+++ scalecodec/__init__.py
@@ -1,2 +1,9 @@
 # Import all type to make sure types classes are registered when RuntimeConfiguration inits.
 from .types import *
+from ._scale_bytes import ScaleBytes
+from .base import (
+    ScaleDecoder,
+    ScaleType,
+    RuntimeConfiguration,
+    RuntimeConfigurationObject,
+)
